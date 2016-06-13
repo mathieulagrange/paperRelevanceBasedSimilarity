98fcc77441f8930583ea1483e7f278a9d8a978b3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-06-13 15:18:39 +0200

    set N to 2^19, start calling scattering transform

--- experiments/supervised/supervised_script.m
+++ experiments/supervised/supervised_script.m
@@ -23,7 +23,7 @@ y = cellfun(@(x) find(strcmp(x, classes)), class_names) - 1;
 % Prepare scattering "architectures", i.e. filter banks and nonlinearities
 clear opts;
 opts{1}.time.nFilters_per_octave = nfo;
-opts{1}.time.size = 131072;
+opts{1}.time.size = 2^19;
 opts{1}.time.T = 16384;
 opts{1}.time.is_chunked = false;
 opts{1}.time.gamma_bounds = [];
@@ -39,3 +39,15 @@ archs = sc_setup(opts);
 % Prepare azimuthal augmentation
 azimuths = linspace(0.0, 1.0, nAzimuths);
 mixing_matrix = cat(1, azimuths, 1.0 - azimuths);
+
+%%
+
+nFiles = length(paths);
+file_index = 1
+%for file_index = 1:nFiles
+    path = paths{file_index};
+    stereo_waveform = audioread(path);
+    multichannel_waveform = stereo_waveform * mixing_matrix;
+    [features, scattergram] = ...
+        multichannel_scattering(multichannel_waveform, archs);
+%end
