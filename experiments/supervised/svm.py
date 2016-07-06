920bbadeaef10ddee65c6c65b3eaa606ddab51ba
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-07-06 02:02:45 +0200

    update svm.py

--- experiments/supervised/svm.py
+++ experiments/supervised/svm.py
@@ -11,44 +11,61 @@ import sklearn.preprocessing
 
 np.set_printoptions(precision=2)
 
-for method in ['time']:
-    for selection in [False]:
-        for compression in [True]:
-            method_str = "Method: " + method
-            if selection:
-                method_str + " + feature selection"
-            if compression:
-                method_str + " + compression"
-            print method_str
+# Load
+print(datetime.datetime.now().time().strftime('%H:%M:%S') + " Loading")
+hdf5_file = h5py.File(
+    "memoized_features/dcase2013_" + method + "Q8_test.mat")
+X = hdf5_file[u'X_test']
+Y = np.ravel(hdf5_file[u'Y_test'])
 
-            # Load
-            print(datetime.datetime.now().time().strftime('%H:%M:%S') +
-                " Loading")
-            hdf5_file = h5py.File("memoized_features/" + method + "_Q=08.mat")
-            hdf5_group = hdf5_file[u'data']
+for augmentation in [True, False], method in ['time', 'timefrequency'],
+    selection in [False, True], integration in ['early', 'true']:
+        method_str = "Method: " + method
+        if augmentation:
+            method_str = method_str + " + augmentation"
+        if selection:
+            method_str = method_str + " + selection"
+        method_str = method_str + " + " + integration + " integration"
+        print method_str
 
-            X_training = hdf5_group[u'X_features_train']
-            X_test = hdf5_group[u'X_features_test']
-            Y_training = np.ravel(hdf5_group[u'Y_train'])
-            Y_test = np.ravel(hdf5_group[u'Y_test'])
+        # Split folds
+        X = np.reshape(X,
+            (X.shape[0],       # ~1k features
+             X.shape[1],       # 128 time
+             X.shape[2],       #   5 azimuths
+             X.shape[3] / 5,   #  20 scenes per fold
+             5,                #   5 folds
+            )
+
+        for fold_id in range(5):
+            Y_training = Y[[fold_id]]
+            Y_test = Y[[np.range(5)!=fold_id]]
 
             # Concatenate azimuths as different training examples
-            X_training = np.reshape(X_training,
-                (X_training.shape[0] * X_training.shape[1],
-                X_training.shape[2], X_training.shape[3]))
-            Y_training = np.repeat(Y_training, 5)
-
+            if augmentation:
+                X_training = X[:, :, :, :, np.range(5)!=fold_id]
+                Y_training = np.repeat(Y_training, 5)
+            else:
+                X_training = X[:, :, [2], :, np.range(5)!=fold_id]
             # Pick central azimuth at test time
-            X_test = X_test[:, 2, :, :]
+            X_test = X[:, :, [2], :, [fold_id]]
 
-            # Sum along the time dimension
-            X_training = np.sum(X_training, 1)
-            X_test = np.sum(X_test, 1)
+            if integration == "early":
+                X_training =
+                    np.sum(X_training, 1)[:, np.newaxis, :, :, :]
+                X_test =
+                    np.sum(X_training, 1)[:, np.newaxis, :, :, :]
+            if integration == "late":
+                Y_training = np.repeat(Y_training, 128)
+                Y_test = np.repeat(Y_test, 128)
+
+            X_training = np.transpose(np.reshape(X_training,
+                X_training.shape[0], np.prod(X_training.shape[1:4])))
+            X_test = np.transpose(np.reshape(X_training,
+                X_training.shape[0], np.prod(X_training.shape[1:4])))
 
             # Discard features with less than 1% of the energy
             if selection:
-                print(datetime.datetime.now().time().strftime('%H:%M:%S') +
-                    " Selection")
                 energies = X_training * X_training
                 feature_energies = np.mean(X_training, axis=0)
                 feature_energies /= np.sum(feature_energies)
@@ -61,49 +78,45 @@ for method in ['time']:
                 X_test = X_test[:, dominant_indices]
 
             # Log transformation
-            if compression:
-                print(
-                    datetime.datetime.now().time().strftime('%H:%M:%S') +
-                    " Compression")
-                X_training = np.log(1e-16 + X_training)
-                X_test = np.log(1e-16 + X_test)
+            X_training = np.log(1e-16 + X_training)
+            X_test = np.log(1e-16 + X_test)
 
-            # Standardize features
-            print(datetime.datetime.now().time().strftime('%H:%M:%S') +
-                " Standardization")
-            scaler = sklearn.preprocessing.StandardScaler().fit(X_training)
-            X_training = scaler.transform(X_training)
-            X_test = scaler.transform(X_test)
-            report = []
-            output_file = open('mdb' + method + 'svm_y.pkl', 'wb')
-            pickle.dump(report, output_file)
-            output_file.close()
+        # Standardize features
+        print(datetime.datetime.now().time().strftime('%H:%M:%S') +
+            " Standardization")
+        scaler = sklearn.preprocessing.StandardScaler().fit(X_training)
+        X_training = scaler.transform(X_training)
+        X_test = scaler.transform(X_test)
+        report = []
+        output_file = open('mdb' + method + 'svm_y.pkl', 'wb')
+        pickle.dump(report, output_file)
+        output_file.close()
 
-            # Train linear SVM
-            print(datetime.datetime.now().time().strftime('%H:%M:%S') +
-                " Training")
-            clf = sklearn.svm.LinearSVC(class_weight="balanced")
-            clf.fit(X_training, Y_training)
+        # Train linear SVM
+        print(datetime.datetime.now().time().strftime('%H:%M:%S') +
+            " Training")
+        clf = sklearn.svm.LinearSVC(class_weight="balanced")
+        clf.fit(X_training, Y_training)
 
-            # Predict and evaluate average miss rate
-            print(datetime.datetime.now().time().strftime('%H:%M:%S') +
-                " Evaluation")
-            Y_training_predicted = clf.predict(X_training)
-            Y_test_predicted = clf.predict(X_test)
-            accuracy =\
-                sklearn.metrics.accuracy_score(Y_test_predicted, Y_test)
-            print "Accuracy = " + str(100 * accuracy)
-            print ""
-            dictionary = {
-                'accuracy': accuracy,
-                'compression': compression,
-                'method': method,
-                'method_str': method_str,
-                'selection': selection,
-                'Y_test': Y_test,
-                'Y_test_predicted': Y_test_predicted,
-                'Y_training': Y_training,
-                'Y_training_predicted': Y_training_predicted}
-            output_file = open(method_str + '.pkl', 'wb')
-            pickle.dump(dictionary, output_file)
-            output_file.close()
+        # Predict and evaluate average miss rate
+        print(datetime.datetime.now().time().strftime('%H:%M:%S') +
+            " Evaluation")
+        Y_training_predicted = clf.predict(X_training)
+        Y_test_predicted = clf.predict(X_test)
+        accuracy =\
+            sklearn.metrics.accuracy_score(Y_test_predicted, Y_test)
+        print "Accuracy = " + str(100 * accuracy)
+        print ""
+        dictionary = {
+            'accuracy': accuracy,
+            'compression': compression,
+            'method': method,
+            'method_str': method_str,
+            'selection': selection,
+            'Y_test': Y_test,
+            'Y_test_predicted': Y_test_predicted,
+            'Y_training': Y_training,
+            'Y_training_predicted': Y_training_predicted}
+        output_file = open(method_str + '.pkl', 'wb')
+        pickle.dump(dictionary, output_file)
+        output_file.close()
