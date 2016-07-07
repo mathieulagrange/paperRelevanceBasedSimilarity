71d4b760963156b039292e89e43072ff6d03ffbd
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-07-07 18:45:34 +0200

    integrate compression, C to pipeline. export Y_predicted in dict

--- experiments/supervised/dcase_svm.py
+++ experiments/supervised/dcase_svm.py
@@ -12,28 +12,28 @@ import sklearn.linear_model
 import sklearn.metrics
 import sklearn.preprocessing
 
-def dcase_svm(octmin, octmax, augmentation, method, selection, integration):
-    if method == "time":
+def dcase_svm(octmin, octmax, augmentation, scattering, compression, integration, C=C):
+    if scattering == "time":
         hdf5_file = h5py.File("memoized_features/dcase2013_timeQ8_test.mat")
         X = hdf5_file["dcase2013_timeQ8_test"]["X_test"]
         freqs = np.ravel(hdf5_file["dcase2013_timeQ8_test"]["freqs"])
         Y = np.ravel(hdf5_file["dcase2013_timeQ8_test"][u'Y_test'])
-    if method == "timefrequency":
+    if scattering == "timefrequency":
         hdf5_file = h5py.File(
             "memoized_features/dcase2013_timefrequencyQ8_test.mat")
         X = hdf5_file["dcase2013_timefrequencyQ8_test"]["X_test"]
         freqs = np.ravel(hdf5_file["dcase2013_timefrequencyQ8_test"]["X_freqs"])
         Y = np.ravel(hdf5_file["dcase2013_timefrequencyQ8_test"][u'Y_test'])
 
-    method_str = method + "_oct" + str(octmin) + "to" + str(octmax)
+    method_str = scattering + "_oct" + str(octmin) + "to" + str(octmax)
+    method_str = method_str + "_" + compression
     if augmentation:
         method_str = method_str + "_augmentation"
-    if selection:
-        method_str = method_str + "_selection"
+    if
     method_str = method_str + "_" + integration
 
-    fmin = 20.0 * (2**octmin)
-    fmax = 20.0 * (2**octmax)
+    fmin = 10.0 * (2**octmin) # in Hz
+    fmax = 10.0 * (2**octmax) # in Hz
     freq_indices = (freqs > fmin) & (freqs < fmax)
     X = X[:, :, :, freq_indices]
 
@@ -89,9 +89,13 @@ def dcase_svm(octmin, octmax, augmentation, method, selection, integration):
             X_test = X_test[:, dominant_indices]
 
         # Log transformation (1e2 is what yields the lowest skewness)
-        medians = np.median(X_training, axis=0)[np.newaxis, :]
-        X_training = np.log1p(1e2 * X_training / medians)
-        X_test = np.log1p(1e2 * X_test / medians)
+        if compression = "logmedian":
+            medians = np.median(X_training, axis=0)[np.newaxis, :]
+            X_training = np.log1p(1e2 * X_training / medians)
+            X_test = np.log1p(1e2 * X_test / medians)
+        if compression = "log"
+            X_training = np.log(X_training)
+            X_test = np.log1p(X_test)
 
         # Standardize features
         scaler = sklearn.preprocessing.StandardScaler().fit(X_training)
@@ -99,7 +103,7 @@ def dcase_svm(octmin, octmax, augmentation, method, selection, integration):
         X_test = scaler.transform(X_test)
 
         # Train linear SVM
-        clf = sklearn.svm.LinearSVC(class_weight="balanced")
+        clf = sklearn.svm.LinearSVC(class_weight="balanced", C=C)
         clf.fit(X_training, Y_training)
 
         # Predict and evaluate average miss rate
@@ -117,6 +121,7 @@ def dcase_svm(octmin, octmax, augmentation, method, selection, integration):
         accuracy =\
             sklearn.metrics.accuracy_score(Y_test_predicted, Y_test)
         accuracies.append(accuracy)
+        Y_predicted.append(Y_test_predicted)
 
     mean_accuracy = np.mean(accuracies)
     std_accuracy = np.std(accuracies)
@@ -127,16 +132,17 @@ def dcase_svm(octmin, octmax, augmentation, method, selection, integration):
         'accuracy_mean' : mean_accuracy,
         'accuracy_std': std_accuracy,
         'augmentation': augmentation,
+        'C': C,
         'fmin': fmin,
         'fmax': fmax,
         'integration': integration,
-        'method': method,
         'method_str': method_str,
         'octmin': octmin,
         'octmax': octmax,
+        'scattering': scattering,
         'selection': selection,
-        'Y_test': Y_test,
-        'Y_test_predicted': Y_test_predicted}
+        'Y': Y,
+        'Y_predicted': Y_predicted}
     return dictionary
 
 cachedir = os.path.expanduser('~/joblib')
