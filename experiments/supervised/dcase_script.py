657d786e12eb1034f4516a708ff32038cf582289
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-07-07 11:17:45 +0200

    start reporting results

--- experiments/supervised/dcase_script.py
+++ experiments/supervised/dcase_script.py
@@ -18,17 +18,15 @@ np.set_printoptions(precision=2)
 
 delayed_dcase_svm = joblib.delayed(cached_dcase_svm)
 
-dictionaries = joblib.Parallel(batch_size=1, n_jobs=-1, verbose=10)(
-    delayed_dcase_svm(
+dictionaries = [
+    cached_dcase_svm(
         octmin,
         octmax,
         augmentation,
         method,
         selection,
         integration)
-    for octmin in [ 0, 1, 2, 3 ]
-    for octmax in [ 10, 9, 8, 7, 6, 5, 4 ]
     for augmentation in [ False, True ]
     for method in [ 'time' ]
     for selection in [ False, True ]
-    for integration in [ "early", "late"])
+    for integration in [ "early", "late"] ]
