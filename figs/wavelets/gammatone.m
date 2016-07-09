4c5547bb6866dad1f7baade9e1b58d3a5149eac1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-07-09 20:46:40 +0200

    better line widths in Fig 1

--- figs/wavelets/gammatone.m
+++ figs/wavelets/gammatone.m
@@ -1,5 +1,5 @@
 nfo = 8;
-line_width = 1.5;
+line_width = 2;
 
 opts{1}.time.nFilters_per_octave = nfo;
 opts{1}.time.size = 2^19;
@@ -20,16 +20,27 @@ psi2_fts = display_bank(archs{2}.banks{1});
 psi1_ft = psi1_fts(:, 40);
 psi1_ift = ifftshift(ifft(psi1_ft));
 psi1_ift = psi1_ift((end/2) + (-300:1000));
-psi1 = [real(psi1_ift),imag(psi1_ift),abs(psi1_ift)];
+psi1 = [real(psi1_ift),imag(psi1_ift)];
+abspsi1 = abs(psi1_ift);
 
 psi2_ft = psi2_fts(:, 11);
 psi2_ift = ifftshift(ifft(psi2_ft));
 psi2_ift = psi2_ift((end/2) + (-3000:10000));
-psi2 = [real(psi2_ift),imag(psi2_ift),abs(psi2_ift)];
+psi2 = [real(psi2_ift),imag(psi2_ift)];
+abspsi2 = abs(psi2_ift);
 
+figure(1);
 plot(psi1, 'LineWidth', line_width)
 axis off
+hold on
+plot(abspsi1, 'LineWidth', 2 * line_width);
+hold off;
 export_fig -transparent gammatone_Q8.png
+
+figure(2);
 plot(psi2, 'LineWidth', line_width);
 axis off
+hold on
+plot(abspsi2, 'LineWidth', 2 * line_width);
+hold off;
 export_fig -transparent gammatone_Q1.png
