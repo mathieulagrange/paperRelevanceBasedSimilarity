f1e4d9addfcd911d8e27a2fdbc075b3f274a6f02
gregoire lafay <gregoirelafay@sfr.fr>
2016-07-09 07:10:44 +0200

    add scripts to generate figures

---
+++ generateFig/unsupervised.m
@@ -0,0 +1,81 @@
+clear all
+addpath(genpath('functions'))
+
+
+
+%% settings
+
+dataset='train'; % test;
+dataPath={['~/projets/ass_features/report/figures/unsupervised_earlyInt_' dataset]};
+freqRange = {'27,5_1000','27,5_2000','27,5_4000','27,5_8000','27,5_16000','27,5_22050'};
+
+linewidth=2;
+markersize=10;
+
+%% load data
+data=cell(1,length(dataPath));
+leg=cell(1,length(dataPath));
+
+for jj=1:length(dataPath)
+    d=load(dataPath{jj});
+    [data{jj},leg{jj}]=parseTable(d.data(1).tables{end});
+end
+
+pak_early=str2double(data{1}(9:15,:));
+
+scat=strcmp('scatT',data{1}(1,:));
+mfcc=strcmp('mfcc',data{1}(1,:));
+
+%% best p@9
+
+ind_best_mfcc=find( mfcc & pak_early(end,:)==max(pak_early(end,mfcc)));
+ind_best_scat=find( scat & pak_early(end,:)==max(pak_early(end,scat)));
+
+best_setting_mfcc=data{1}(:,ind_best_mfcc);
+best_setting_scat=data{1}(:,ind_best_scat(1));
+
+freq=strcmp('27,5_1000',data{1}(2,:));
+mfcc40=strcmp('40',data{1}(3,:));
+c0=strcmp('1',data{1}(4,:));
+scat_log=strcmp('1',data{1}(5,:));
+stand=strcmp('stand',data{1}(6,:));
+gaussian=strcmp('gaussian_full',data{1}(7,:));
+nn_5=strcmp('0.05',data{1}(8,:));
+nn_15=strcmp('0.15',data{1}(8,:));
+
+
+pak_early_mfcc=pak_early(:,mfcc & gaussian & ~stand & mfcc40 & c0 & nn_15 & freq);
+pak_early_scat=pak_early(:,scat & gaussian & ~stand  & nn_15 & ~scat_log);
+pak_early_scat_log=pak_early(:,scat & gaussian & ~stand  & nn_15 & scat_log);
+
+%%
+figure(1)
+clf
+plot(pak_early_mfcc,'o-','markersize',markersize,'linewidth',linewidth)
+hold on
+plot(pak_early_scat_log,'+-','markersize',markersize,'linewidth',linewidth)
+hold off
+legend({'ei, mfcc','ei, time-scattering'},'interpreter','none')
+set(gca,'xtick',1:7,'xticklabel',3:9)
+xlabel('k')
+ylabel('p@k')
+disp('')
+
+figure(2)
+clf
+plot(pak_early_scat,'o-','markersize',markersize,'linewidth',linewidth)
+hold on
+plot(pak_early_scat_log,'+-','markersize',markersize,'linewidth',linewidth)
+hold off
+legend({'ei, w/o log','ei, w/ log'},'interpreter','none')
+set(gca,'xtick',1:7,'xticklabel',3:9)
+xlabel('k')
+ylabel('p@k')
+disp('')
+
+%% print
+figOpt.fontsize=16;
+figOpt.height=35;
+figOpt.width=20;
+
+printFigures([1 2],['~/papers/paperStructureScene16/paper/gfx/unsupervised_' dataset],figOpt)
