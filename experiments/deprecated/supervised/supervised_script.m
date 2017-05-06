2bf56eb7eabeef2d300805d1984743a823072c26
Vincent <vincent.lostanlen@nyu.edu>
2017-05-06 18:46:52 -0400

    replace path to datasets

--- experiments/deprecated/supervised/supervised_script.m
+++ experiments/deprecated/supervised/supervised_script.m
@@ -1,6 +1,7 @@
 % Before running this script, the library scattering.m must have been added
 % to the MATLAB path
-dataset_path = '~/datasets/dcase2013/scenes_stereo';
+%dataset_path = '~/datasets/dcase2013/scenes_stereo';
+dataset_path = '/scratch/vl1019/datasets/scenes_stereo';
 Q1 = 8;
 
 dcase_scattering(dataset_path, Q1);
