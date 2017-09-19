2fb3d4250f282943563f0778866fbffa71e2dbf2
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2017-09-19 15:47:00 +0200

    move on to dcase2016

--- experiments/unsupervised/taun1features.m
+++ experiments/unsupervised/taun1features.m
@@ -11,7 +11,7 @@ function [config, store, obs] = taun1features(config, setting, data)
 % Date: 17-Dec-2016
 
 % Set behavior for debug mode
-if nargin==0, unsupervised('do', 1, 'mask', {}); return; else store=[]; obs=[]; end
+if nargin==0, unsupervised('do', 1, 'mask', {2,1}); return; else store=[]; obs=[]; end
 
 %% setting
 store.xp_settings.sr=44100;
@@ -69,14 +69,34 @@ switch setting.features
 
         
         %% select sound
-        
-        fileId = fopen([config.inputPath 'sampleList_' setting.dataset '.txt']);
-        store.xp_settings.sounds=textscan(fileId,'%s');store.xp_settings.sounds=store.xp_settings.sounds{1};
+        switch setting.dataset
+            case '2013'
+        fileId = fopen([config.inputPath 'dcase2013/'  'sampleList_train.txt']);
+        store.xp_settings.sounds=textscan(fileId,'%s');
+        store.xp_settings.sounds=store.xp_settings.sounds{1};
         fclose(fileId);
         
         %% load sound
         
         store.xp_settings.soundIndex = 1:100;
+            case '2016'
+                fid = fopen([config.inputPath 'dcase2016/meta.txt' ]);
+                C = textscan(fid, '%s\t%s');
+                fclose(fid);
+                
+                store.xp_settings.sounds = C{1};
+                typo = C{2};
+                store.xp_settings.soundIndex=[];
+                store.xp_settings.classes = typo(1);
+                for k=1:length(typo)
+                   if  (strfind(store.xp_settings.sounds{k}, '_0_30'))
+                      store.xp_settings.soundIndex(end+1) = k; 
+                   end
+                   if ~strcmp(typo{k}, store.xp_settings.classes)
+                       store.xp_settings.classes{end+1} = typo{k};
+                   end
+                end
+        end
         
         % init
         eval(['store.features.' setting.features ' = [];']);
@@ -88,7 +108,7 @@ switch setting.features
         
         for jj=1:length(store.xp_settings.soundIndex)
             
-            [signal,fs]=audioread([config.inputPath store.xp_settings.sounds{store.xp_settings.soundIndex(jj)}  '.wav']);
+            [signal,fs]=audioread([config.inputPath 'dcase' setting.dataset '/' store.xp_settings.sounds{store.xp_settings.soundIndex(jj)}  '.wav']);
             
             if fs~=store.xp_settings.sr
                 error('issue with sample rate')
@@ -111,11 +131,14 @@ switch setting.features
             
             store.soundIndex(jj)=store.xp_settings.soundIndex(jj);
             store.indSample=[store.indSample ones(1,ftrs.size)*store.soundIndex(jj)];
-            
+            switch setting.dataset
+                case '2013'
             soundname=store.xp_settings.sounds{store.xp_settings.soundIndex(jj)}(1:end-2);
             
             soundname=soundname(strfind(soundname,'/')+1:end);
             store.class(jj)=find(strcmp(soundname,store.xp_settings.classes));
-            
+                case '2016'
+                store.class(jj)=find(strcmp(typo{jj}, store.xp_settings.classes));    
+            end
         end
 end
