ba45e77472f16938bcdfe3dd0ea241d885724b62
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2018-02-22 10:36:33 +0100

    imporved figures

--- paper/figures/mat2csv.m
+++ paper/figures/mat2csv.m
@@ -1,10 +1,22 @@
 
-files = dir('./*.mat')
+files = dir('./*.mat');
 
 for k=1:length(files)
     clear data
-   load(files(k).name) 
-   if (exist('data', 'var'))
-     csvwrite([files(k).name(1:end-3), 'csv'], data.meanData); 
-   end
+    load(files(k).name)
+    if (exist('data', 'var'))
+        csvwrite([files(k).name(1:end-3), 'csv'], data.meanData);
+    end
+    
+    h=openfig([files(k).name(1:end-3) 'fig']);
+    set(h, 'visible', 'on')
+    ylabel('p@k (%)');
+    legend('-DynamicLegend');
+    lines = findobj(gcf,'Type','Line');
+    for i = 1:numel(lines)
+        lines(i).LineWidth = 2.0;
+    end
+    axis([1 9 10 100]);
+    disp('edit');
+    saveas(gcf, [files(k).name(1:end-3) 'png'])
 end
